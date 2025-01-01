0db3846e71fcc34fbb9c7033652a75fd1a5c03bd
sandesh <sandeshbhatta218@gmail.com>
2025-01-01 16:33:14 +0545

    2- Handling Text Input

--- App.js
+++ App.js
@@ -1,32 +1,25 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
 
-function Cat(props) {
-  const [isHungry, setIsHungry] = useState(true);
+export default function PizzaTranslator() {
+  const [text, setText] = useState('');
   return (
     <View>
-      <Text style={styles.container}>
-        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}
-      </Text>
-      <Button
-        onPress={() => {
-          setIsHungry(false);
-        }}
-        disabled={!isHungry}
-        title={isHungry ? 'Give me some Food' : 'Thank You'}
+      <TextInput style={styles.container}
+        placeholder="Type Here to translate"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
       />
+
+      <Text>
+        {text
+          .split('')
+          .map(word => word && '🍕')
+          .join('')}
+      </Text>
     </View>
   );
-};
-
-export default function Cafe() {
-  return (
-    <>
-      <Cat name="milo" />
-      <Cat name="silo" />
-    </>
-  );
-};
+}
 
 
 const styles = StyleSheet.create({
@@ -39,6 +32,35 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
 });
+
+/*Props and state*/
+
+// function Cat(props) {
+//   const [isHungry, setIsHungry] = useState(true);
+//   return (
+//     <View>
+//       <Text style={styles.container}>
+//         I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}
+//       </Text>
+//       <Button
+//         onPress={() => {
+//           setIsHungry(false);
+//         }}
+//         disabled={!isHungry}
+//         title={isHungry ? 'Give me some Food' : 'Thank You'}
+//       />
+//     </View>
+//   );
+// };
+
+// export default function Cafe() {
+//   return (
+//     <>
+//       <Cat name="milo" />
+//       <Cat name="silo" />
+//     </>
+//   );
+// };
 
 
 

7d83284fa2fd849f50c12d0921b75186efc2c2c1
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-06 15:54:02 +0545

    7- Register Form

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default function Flex() {
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'row',
-        },
-      ]}>
-      <View style={{ flex: 1, backgroundColor: 'red' }} />
-      <View style={{ flex: 2, backgroundColor: 'darkorange' }} />
-      <View style={{ flex: 3, backgroundColor: 'green' }} />
+    <View style={styles.container}>
+      <Text style={styles.text}>Register</Text>
+      <TextInput
+        placeholder='Enter Name' />
+      <TextInput
+        placeholder='Enter Email' />
+      <TextInput
+        placeholder='Enter Number' />
+
+      <Button title='Click Here!' />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    margin: 80,
     flex: 1,
     padding: 20,
   },
+  text: {
+    textAlign: 'center'
+  }
 });

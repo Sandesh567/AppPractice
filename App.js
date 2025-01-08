7973c1ccf76dc42197af51923130c345ab06d474
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-08 16:48:47 +0545

    9- Registration Form

--- App.js
+++ App.js
@@ -1,47 +1,116 @@
-import React, {useEffect} from 'react';
-import {Animated, Text, View, useAnimatedValue} from 'react-native';
+import React, { useState } from "react";
+import { View, ScrollView, Text, TextInput, StyleSheet } from "react-native";
 
-const FadeInView = props => {
-  const fadeAnim = useAnimatedValue(0); // Initial value for opacity: 0
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmationPassword, setConfirmationPassword] = useState('');
+  const [name, setName] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [breed, setBreed] = useState('');
+  const [toy, setToy] = useState('');
 
-  useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 10000,
-      useNativeDriver: true,
-    }).start();
-  }, [fadeAnim]);
+
+
+  function confirmPasswordMatch(props) {
+    const { nativeEvent: { text } } = props;
+
+    if (text !== password) {
+      alert('Password do not match, Try again');
+    }
+  }
 
   return (
-    <Animated.View // Special animatable View
-      style={{
-        ...props.style,
-        opacity: fadeAnim, // Bind opacity to animated value
-      }}>
-      {props.children}
-    </Animated.View>
-  );
-};
-
-// You can then use your `FadeInView` in place of a `View` in your components:
-export default () => {
-  return (
-    <View
+    <ScrollView
       style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#ecf0f1",
       }}>
-      <FadeInView
-        style={{
-          width: 250,
-          height: 50,
-          backgroundColor: 'powderblue',
-        }}>
-        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>
-          Fading in
-        </Text>
-      </FadeInView>
+      <InputWithLabel
+        label="Email"
+        placeholder="Type your email here!"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <InputWithLabel
+        label="Password"
+        placeholder="Type your Password here!"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <InputWithLabel
+        label="Confirm Password"
+        placeholder="Confirm Password here!"
+        value={confirmationPassword}
+        onChangeText={setConfirmationPassword}
+        secureTextEntry
+        onSubmitEditing={confirmPasswordMatch}
+      />
+      <InputWithLabel
+        label="Pet Name"
+        placeholder="Type your pet's name here!"
+        value={name}
+        onChangeText={setName}
+      />
+      <InputWithLabel
+        label="Pet Date of Birth"
+        placeholder="Type your Pet's Date of Birth here!"
+        value={birthday}
+        onChangeText={setBirthday}
+      />
+      <InputWithLabel
+        label="Breed"
+        placeholder="Type your pet's breed here!"
+        value={breed}
+        onChangeText={setBreed}
+      />
+      <InputWithLabel
+        label="Toy"
+        placeholder="Type your pet's favorite toy"
+        value={toy}
+        onChangeText={setToy}
+      />
+
+    </ScrollView>
+  );
+}
+
+function InputWithLabel(props) {
+  const { label, placeholder, value, onChangeText, secureTextEntry, onSubmitEditing } = props;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{label}</Text>
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+        onSubmitEditing={onSubmitEditing}
+        style={styles.input}
+      />
     </View>
   );
-};
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    margin: 18
+  },
+  text: {
+    padding: 8,
+    fontSize: 18,
+    color: 'blue',
+  },
+  input: {
+    padding: 8,
+    fontSize: 18,
+    borderColor: 'red',
+
+  }
+});
+
+export default App;

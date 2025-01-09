1357eee1e3e70b299bb21cf6e82f7cb608b5499a
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-09 17:40:41 +0545

    10-Cards

--- App.js
+++ App.js
@@ -1,116 +1,39 @@
-import React, { useState } from "react";
-import { View, ScrollView, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 
-function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmationPassword, setConfirmationPassword] = useState('');
-  const [name, setName] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [breed, setBreed] = useState('');
-  const [toy, setToy] = useState('');
+const App = () => (
+  <View
+    style={styles.layout}
+  >
+    <View
+      style={styles.card}
+    />
 
+  <View
+      style={styles.card}
+    />
+  </View>
+);
 
-
-  function confirmPasswordMatch(props) {
-    const { nativeEvent: { text } } = props;
-
-    if (text !== password) {
-      alert('Password do not match, Try again');
-    }
-  }
-
-  return (
-    <ScrollView
-      style={{
-        flex: 1,
-        backgroundColor: "#ecf0f1",
-      }}>
-      <InputWithLabel
-        label="Email"
-        placeholder="Type your email here!"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <InputWithLabel
-        label="Password"
-        placeholder="Type your Password here!"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <InputWithLabel
-        label="Confirm Password"
-        placeholder="Confirm Password here!"
-        value={confirmationPassword}
-        onChangeText={setConfirmationPassword}
-        secureTextEntry
-        onSubmitEditing={confirmPasswordMatch}
-      />
-      <InputWithLabel
-        label="Pet Name"
-        placeholder="Type your pet's name here!"
-        value={name}
-        onChangeText={setName}
-      />
-      <InputWithLabel
-        label="Pet Date of Birth"
-        placeholder="Type your Pet's Date of Birth here!"
-        value={birthday}
-        onChangeText={setBirthday}
-      />
-      <InputWithLabel
-        label="Breed"
-        placeholder="Type your pet's breed here!"
-        value={breed}
-        onChangeText={setBreed}
-      />
-      <InputWithLabel
-        label="Toy"
-        placeholder="Type your pet's favorite toy"
-        value={toy}
-        onChangeText={setToy}
-      />
-
-    </ScrollView>
-  );
-}
-
-function InputWithLabel(props) {
-  const { label, placeholder, value, onChangeText, secureTextEntry, onSubmitEditing } = props;
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{label}</Text>
-      <TextInput
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
-        secureTextEntry={secureTextEntry}
-        onSubmitEditing={onSubmitEditing}
-        style={styles.input}
-      />
-    </View>
-  );
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 12,
-    margin: 18
+export const styles = StyleSheet.create({
+  layout:{
+      flex: 1, 
+      backgroundColor: "#e5e5e5",
+      justifyContent: "center"
   },
-  text: {
-    padding: 8,
-    fontSize: 18,
-    color: 'blue',
-  },
-  input: {
-    padding: 8,
-    fontSize: 18,
-    borderColor: 'red',
-
+  card:{
+        width: 100,
+        height: 100,
+        backgroundColor: 'white',
+        margin: 16,
+        borderRadius: 2,
+        shadowColor: "black",
+        shadowOpacity: 0.3,
+        shadowRadius: 1,
+        shadowOffset: { height: 1, width: 0.3 },
   }
 });
 
 export default App;
+
+

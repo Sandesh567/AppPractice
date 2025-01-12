fb2a626de24e93344c26c36acb18f733eff0019f
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-12 17:29:33 +0545

    14-Stack Navigation with Button

--- App.js
+++ App.js
@@ -1,43 +1,55 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// Installing Stack Navigation
+/**
+ *npm install @react-navigation/native @react-navigation/native-stack
+ */
+
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const FeedScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.layout}>
+      <Text style={styles.title}>Feed</Text>
+      <Button
+        title="Go to catalog"
+        onPress={() => navigation.navigate('Catalog')}
+      />
+    </View>
+  );
+};
+
+const CatalogScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Catalog</Text>
+  </View>
+);
 
 const Stack = createNativeStackNavigator();
 
-
+export const AppNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Feed" component={FeedScreen} />
+    <Stack.Screen name="Catalog" component={CatalogScreen} />
+  </Stack.Navigator>
+);
 
 const App = () => (
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Feed" component={FeedScreen} options={{ title: 'Home' }} />
-      <Stack.Screen name="Catalog" component={CatalogScreen} />
-    </Stack.Navigator>
+    <AppNavigator />
   </NavigationContainer>
-)
+);
 
 export default App;
-
-
-export const FeedScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title}>First screen</Text>
-  </View>
-)
-
-export const CatalogScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title} >Second Screen</Text>
-  </View>
-)
-
-
 
 const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 32,
